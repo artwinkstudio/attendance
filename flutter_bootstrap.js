f36a2d923b0476b58fcac747c4418d4a58395379
paklong <paklong@users.noreply.github.com>
2024-10-27 04:32:14 +0000

    deploy: 42bab4b6170f211d0483111747e4257703baa31e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "251239793"
+    serviceWorkerVersion: "1460104162"
   }
 });
